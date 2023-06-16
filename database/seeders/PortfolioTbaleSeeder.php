83a0b413c52b24c96ffeb8b0292471df90e717c7
GiacomoArena <giacomoarena28@gmail.com>
2023-06-16 16:50:07 +0200

    make migration and seeder

---
+++ database/seeders/PortfolioTbaleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Portfolio;
+
+class PortfolioTbaleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $new_portfolio = new Portfolio();
+    }
+}
