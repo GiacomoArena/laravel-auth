6e5e985e1f847566f4fed292328dfa7bb1e40299
GiacomoArena <giacomoarena28@gmail.com>
2023-06-16 16:20:07 +0200

    update commit

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,5 +1,27 @@
 <aside class="bg-white shadow-lg">
 
-    <h1>aside</h1>
+        <a href="#" class="text-center">
+            <h3><i class="fa-solid fa-circle-chevron-down"></i></h3>
+        </a>
+
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fa-solid fa-users"></i>
+                    Portfolios</a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-solid fa-user-plus"></i>
+                    Add Portfolio
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.home')}}">
+                    <i class="fa-solid fa-house-user"></i>
+                    Home
+                </a>
+            </li>
+        </ul>
 
 </aside>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>BoolFolio</title>
 
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==' crossorigin='anonymous'/>
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
